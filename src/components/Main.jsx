f44a4d8bce9f55eef930af89bf04940450174aa7
Gustavo <contato.gustavofernandes@hotmail.com>
2022-01-22 06:04:19 -0300

    added style and function to abonner button

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import Link from 'next/link'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  /* background-color: purple; */
   grid-area: main;
   padding: 10px 10px;
 
@@ -17,6 +17,7 @@ const EachPost = styled.div`
   margin: 20px;
   width: 700px;
   margin: 20px auto;
+  margin-bottom: 200px;
 
   display: grid;
   grid-template-columns: 70px 1fr 150px;
@@ -29,30 +30,22 @@ const EachPost = styled.div`
 const TopComp = styled.div`
   grid-area: top;
   background-color: white;
-  /* display: flex;
-  align-items: center; */
   border-bottom: 1px solid #00000046;
-  /* margin-left: 20px; */
   margin: 0 20px;
   padding-right: 20px;
   /* display: flex; */
   /* justify-content: flex-end; */
-  /* align-items: center; */
-  /* width: 100%; */
-  /* flex-wrap: wrap; */
 
   p {
     display: inline-block;
     margin: 10px 0px;
     margin-right: 10px;
-    /* margin-left: 20px; */
-    /* padding */
   }
 `
 
 const Div1 = styled.div`
   small {
-    font-size: 12px;
+    font-size: 14px;
     margin: 4px;
   }
 `
@@ -76,9 +69,11 @@ const MiddleComp = styled.div`
 const Bt = styled.button`
   width: 100px;
   height: 30px;
-  color: #fe2355;
+  color: ${(props) => (props.flag ? '#fe3255' : 'white')};
+  background-color: ${(props) => (props.flag ? 'white' : '#fe2355')};
+  /* background-color: white; */
+  /* color: #fe2355; */
   font-size: 18px;
-  background-color: white;
   border: 1.2px solid #fe2355;
   text-align: center;
   border-radius: 4px;
@@ -86,52 +81,72 @@ const Bt = styled.button`
   cursor: pointer;
 
   :hover {
-    background-color: #fe235623;
+    background-color: #df1948;
+    color: white;
   }
 `
 
+function ChaquePost({ video, flag, setFlag }) {
+  return (
+    <EachPost>
+      <TopComp>
+        <p style={{ fontWeight: '700', fontSize: '13px' }}>
+          <Link href='/'>shalow_person2000</Link>
+        </p>
+        <small style={{ fontWeight: '300', fontSize: '11px' }}>
+          Shalow Person 🌷
+        </small>
+        <Div1>
+          <small>#it-was-hard-to-find-these-interesting-videos-to-post</small>
+          <small>#tiktok-makes-no-sense</small>
+          <small>#boring</small>
+          <small>#regret-cloning-this</small>
+          <small>
+            <b>#never again</b>
+          </small>
+        </Div1>
+      </TopComp>
+      <LeftComp>
+        <div
+          style={{
+            width: '70px',
+            height: '70px',
+            borderRadius: '35px',
+            backgroundColor: 'black',
+            marginTop: '10px',
+          }}
+        />
+      </LeftComp>
+      <RightComp>
+        <Bt onClick={(e) => setFlag(!flag)} flag={flag}>
+          {flag ? "S'abonner" : 'Abonné'}
+        </Bt>
+      </RightComp>
+      <MiddleComp>
+        <video
+          width='480'
+          loop='true'
+          autoPlay='autoplay'
+          muted
+          style={{ pointerEvents: 'none' }}
+        >
+          <source src={video} />
+        </video>
+      </MiddleComp>
+    </EachPost>
+  )
+}
+
 function Main() {
+  const [one, setOne] = useState(true)
+  const [two, setTwo] = useState(false)
+  const [three, setThree] = useState(false)
+
   return (
     <Container>
-      <EachPost>
-        <TopComp>
-          <p style={{ fontWeight: '700', fontSize: '13px' }}>
-            shalow_person2000
-          </p>
-          <small style={{ fontWeight: '300', fontSize: '11px' }}>
-            Shalow Person 🌷
-          </small>
-          <Div1>
-            <small>#it-was-hard-to-find-these-interesting-videos-to-post</small>
-            <small>#tiktok-makes-no-sense</small>
-            <small>#stupid</small>
-            <small>#boring</small>
-            <small>#regret-cloning-this</small>
-            <small>
-              <b>#never again</b>
-            </small>
-          </Div1>
-        </TopComp>
-        <LeftComp>
-          <div
-            style={{
-              width: '70px',
-              height: '70px',
-              borderRadius: '35px',
-              backgroundColor: 'black',
-              marginTop: '10px',
-            }}
-          />
-        </LeftComp>
-        <RightComp>
-          <Bt>S'abonner</Bt>
-        </RightComp>
-        <MiddleComp>
-          <video width='480' loop='true' autoPlay='autoplay' muted>
-            <source src='./videos/video1.mp4' />
-          </video>
-        </MiddleComp>
-      </EachPost>
+      <ChaquePost video='./videos/video3.mp4' flag={one} setFlag={setOne} />
+      <ChaquePost video='./videos/video2.mp4' flag={two} setFlag={setTwo} />
+      <ChaquePost video='./videos/video1.mp4' flag={three} setFlag={setThree} />
     </Container>
   )
 }
